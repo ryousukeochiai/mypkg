e6e511c8592319e600c07757e20ff5a9876a4211
Ryousuke Ochiai <s22C1028FL@s.chibakoudai.jp>
2024-01-11 07:19:49 +0900

    fix command

--- test/test.bash
+++ test/test.bash
@@ -8,11 +8,27 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-export MYPKG_TEST_FIXED_DATE="2024-01-01"
-timeout 10 ros2 launch mypkg DOW_talk_listen.launch.py > /tmp/DOW_mypkg.log
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-	grep 'Listen: 10'
-cat /tmp/DOW_mypkg.log |
-	grep '2024/01/11 (Thu)'
+cat /tmp/mypkg.log | grep 'Listen: 5'
+
+timeout 5 ros2 launch mypkg DOW_talk_listen.launch.py > /tmp/DOW_mypkg.log
+
+cat /tmp/DOW_mypkg.log #| grep ' (Mon)\| (Tue)\| (Wed)\| (Thu)\| (Fri)\| (Sat)\| (Sun)'
+
+judge=false
+
+for day in Mon Tue Wed Thu Fri Sat Sun; do
+    if cat /tmp/DOW_mypkg.log | grep -q " ($day)"; then
+        judge=true
+    else
+        judge=false
+        break
+    fi
+done
+
+if $judge; then
+    exit 0
+else
+    exit 1
+fi
